b1c2a2d8ba1c5e62024e9527d77a6ade258fe885
APDP <ap.duplessis@usask.ca>
2023-04-02 22:59:06 -0600

    Server code from previous work to build from; started connecting to new db files

---
+++ backend/server.js
@@ -0,0 +1,90 @@
+'use strict';
+
+// Load packages
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser')
+const cors = require('cors');
+
+const jsonParser = bodyParser.json()
+
+// Server
+const app = express();
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+    maxAge: 600
+}));
+
+// Connection variables
+const PORT = 3080;
+const HOST = '0.0.0.0';
+
+// Database variables
+let dbConnection = require('./dbInit')
+const { addUser, verifyUser, getUserID, getUserData } = require('./dbUsers')
+
+// Routes
+
+/**
+ * Check that db has been created
+ */
+app.route('/init')
+    .post((req, res, next) => {
+        console.log("db check");
+    })
+
+/**
+ * post: insert new data into initial table
+ */
+app.route('/addPost')
+    .post(jsonParser, (req, res, next) => {
+        // Extract post components
+        let topic = req.body.topic;
+        let data = req.body.data;
+        // Insert into table
+        dbConnection.query(`INSERT INTO posts (topic, data) VALUES
+        ('${topic}', '${data}')`);
+
+        res.send("Posted");
+    })
+    .patch((req, res, next) => {
+        // Space to update existing posts
+    })
+
+/**
+ * get: retrieve all posts from table
+ * patch: retrieve newest posts
+ */
+app.route('/getPosts')
+    .get((req, res, next) => {
+        dbConnection.query(`SELECT * from posts`, (err, results) => {
+            if (err) {
+                console.log("Cannot retrieve posts");
+                console.log(err);
+                res.send(err);
+            }
+            else {
+                res.send(results);
+            }
+        });
+    })
+    .patch(jsonParser, (req, res, next) => {
+        // Extract search parameters
+        let latestPostID = req.body.latest;
+        // Get new posts
+        dbConnection.query(`SELECT * from posts WHERE id > ${latestPostID}`, (err, results) => {
+            if (err) {
+                console.log("Cannot retrieve posts");
+                console.log(err);
+                res.send(err);
+            }
+            else {
+                res.send(results);
+            }
+        });
+    })
+
+app.listen(PORT, HOST);
+console.log('Server up and running');
