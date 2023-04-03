7d7914c39645bc5c3f02d07c3b06323cc314ae66
APDP <ap.duplessis@usask.ca>
2023-04-03 15:13:18 -0600

    Added react-router-dom and added Routes to new pages

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import { Landing } from './Landing';
+import { Channel } from './Channel';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <Router>
+            <Link to="/channelTest">  <button onClick={() => console.log("Channel Test")}> Show Channel X </button> </Link>
+            <Routes>
+              <Route exact path='/' element={<Landing/>} />
+              <Route path="/channelTest" element={<Channel name = {"test"}/>} />
+            </Routes>
+          </Router>
+        </div>
+      
       </header>
     </div>
   );
