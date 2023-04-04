cdd12ebba4ca273b2a5c336755af20a95b9ac02a
APDP <ap.duplessis@usask.ca>
2023-04-04 13:38:01 -0600

    Display ChannelPreviews for all channels

---
+++ react-frontend/src/ShowChannels.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ChannelPreview } from './ChannelPreview';
+
+import './ShowChannels.css';
+
+export const ShowChannels = ({channels}) => {
+    let channelList = [];
+    for (let i=0; i< channels.length; i++) {
+        let channelData = channels[i];
+        let channelPreview = React.createElement(ChannelPreview, {channelName: channelData.name, channelLink: channelData.link});
+        channelList.push(channelPreview);
+    }
+    return (
+        <div className='channels-container'>
+            {channelList.map(channelPreview => (
+                <div className='channel-box' key={channelPreview.props.channelName}>
+                    {channelPreview}
+                </div>
+            ))}
+        </div>
+    );
+}
