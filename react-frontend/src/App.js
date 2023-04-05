88bb21966db0136dff196e9a626e8ef6947ffb77
APDP <ap.duplessis@usask.ca>
2023-04-04 20:08:47 -0600

    Removed react logo and adjusted page to fill window

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -44,7 +43,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <div className='primary-container'>
           <Router>
             <Routes>
