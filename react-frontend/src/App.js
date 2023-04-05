fa60e38e53c190a9ec1faa690c2d7eac61feb763
APDP <ap.duplessis@usask.ca>
2023-04-04 19:46:49 -0600

    Updated App to show Dashboard which will be used to display all page content; adjusted width to fill screen.

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,21 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 import { getDB, getChannels } from './services/DataService';
 import { verifyUser, addUser } from './services/UserService';
 
-import { Login } from './Login';
-import { Landing } from './Landing';
-import { Channel } from './Channel';
-import { ChannelPreview } from './ChannelPreview';
+import { Dashboard } from './Dashboard';
 
 // Icons
 import {library} from '@fortawesome/fontawesome-svg-core';
 import * as Icons from '@fortawesome/free-solid-svg-icons';
-import { ShowChannels } from './ShowChannels';
 
 const iconList = Object.keys(Icons)
   .filter((key) => key !== 'fas' && key !== 'prefix')
@@ -49,13 +45,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>
+        <div className='primary-container'>
           <Router>
-            <Link to="/channelTest">  <button onClick={() => console.log("Channel Test")}> Show Channel X </button> </Link>
             <Routes>
-              {/* <Route exact path='/' element={<Landing/>} /> */}
-              <Route exact path='/' element={<ShowChannels channels={getChannels}/>} />
-              <Route path="/channelTest" element={<Channel name={"test"}/>} />
+              <Route exact path='/' element={<Dashboard getChannels={getChannels}/>} />
             </Routes>
           </Router>
         </div>
