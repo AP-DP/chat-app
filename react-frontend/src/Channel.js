8c840f2db0d5b1dbeeec63d0cd45d94c50fb0163
APDP <ap.duplessis@usask.ca>
2023-04-05 10:15:14 -0600

    Added input field to add messages to a channel.

--- react-frontend/src/Channel.js
+++ react-frontend/src/Channel.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import {useState} from 'react';
 
 import './Channel.css';
 
-export const Channel = ({name}) => {
+import { Message } from "./Message";
+
+export const Channel = ({name, posts}) => {
+
+    const [getMessage, setMessage] = useState('');
+
+    let messages = [];
+
+    for (let i=0; i< posts.length; i++) {
+        let messageData = posts[i];
+        let message = React.createElement(Message, {
+            author: messageData.author, 
+            message: messageData.content, 
+            timestamp: messageData.timestamp
+        });
+        messages.push(message);
+    }
+
     return (
-        <h2 className='channel'> {name}: </h2>
+        <>
+            <h3 className='channel'> {name}: </h3>
+            <div>
+
+            </div>
+            <div>
+                <input type="text" maxLength="80" placeholder="Message" value={getMessage} 
+                onChange={e => setMessage(e.target.value)} /><br/>
+            </div>
+            <button onClick={(e) => {
+                let message = getMessage;
+                console.log(message);
+                if (message.length === 0) {
+                    alert("Please add a message to post.")
+                }
+                else {
+                    // Send data to be added to db
+                    setMessage(message)
+                    // Reset input fields
+                    setMessage("")
+                }
+            }}> Submit</button>
+        </>
     );
 }
